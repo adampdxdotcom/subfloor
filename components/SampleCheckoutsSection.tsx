d8de00f23d83885be280184ede9a24264a576bee
Adam Michaels <adam@adampdx.com>
2025-12-17 11:50:17 -0800

    Minor Update to New project

--- components/SampleCheckoutsSection.tsx
+++ components/SampleCheckoutsSection.tsx
@@ -16,7 +16,10 @@ interface SampleCheckoutsSectionProps {
 const getThreeDaysFromNowISO = () => {
     const date = new Date();
     date.setDate(date.getDate() + 3);
-    return date.toISOString().split('T')[0];
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
 };
 
 const formatVariantName = (productName: string, variantName: string) => {
