5e6d43174532703449ced7a0c9acaec2ef90335a
Adam Michaels <adam@adampdx.com>
2026-01-21 06:25:13 -0800

    Increase file upload size

--- server/index.js
+++ server/index.js
@@ -207,7 +207,7 @@ app.options('*', (req, res) => {
     res.sendStatus(204);
 });
 
-app.use(express.json()); 
+app.use(express.json({ limit: '50mb' })); 
 app.use('/uploads', express.static(uploadsDir));
 
 // --- NEW DEBUGGER FOR AUTH ---
